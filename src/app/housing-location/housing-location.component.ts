c25e41598cd9ef5d6be8b06552b21756041a5a5c
Azizur Rahman <azizur.rahman@orbitax.com>
2024-10-27 18:40:29 +0600

    upload angular code

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
+
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section class="listing">
+      <img class="listing-photo">
+      <h2 class="listing-heading"></h2>
+      <p class="listing-location"></p>
+    </section>
+  `,
+  styleUrls: ['./housing-location.component.css']
+})
+export class HousingLocationComponent {
+
+}
